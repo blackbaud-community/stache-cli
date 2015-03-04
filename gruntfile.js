5981953adc87375ae5aca610577850934b9e55db
Bobby <bobby.earl@blackbaud.com>
2015-03-04 15:42:43 -0500

    Updated path to boilerplate

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         files: [
           {
             expand: true,
-            cwd: 'boilerplate/',
+            cwd: 'stache-boilerplate/',
             src: '**',
             dest: '<%= boilerplateDest %>'
           }
