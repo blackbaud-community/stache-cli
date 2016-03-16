dae98cc2ac8dd95479d80ac7759b99c663ede987
Bobby Earl <bobby.earl@blackbaud.com>
2016-03-16 09:46:19 -0400

    Running setup

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -23,6 +23,47 @@ notifySlack() {
 }
 
 # Necessary in the Azure Environment
+setup () {
+  SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
+  SCRIPT_DIR="${SCRIPT_DIR%/*}"
+  ARTIFACTS=$SCRIPT_DIR/../artifacts
+  KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"}
+
+  if [[ ! -n "$DEPLOYMENT_SOURCE" ]]; then
+    DEPLOYMENT_SOURCE=$SCRIPT_DIR
+  fi
+
+  if [[ ! -n "$NEXT_MANIFEST_PATH" ]]; then
+    NEXT_MANIFEST_PATH=$ARTIFACTS/manifest
+
+    if [[ ! -n "$PREVIOUS_MANIFEST_PATH" ]]; then
+      PREVIOUS_MANIFEST_PATH=$NEXT_MANIFEST_PATH
+    fi
+  fi
+
+  if [[ ! -n "$DEPLOYMENT_TARGET" ]]; then
+    DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
+  else
+    KUDU_SERVICE=true
+  fi
+
+  if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
+    # Install kudu sync
+    echo Installing Kudu Sync
+    npm install kudusync -g --silent
+    exitWithMessageOnError "npm failed"
+
+    if [[ ! -n "$KUDU_SERVICE" ]]; then
+      # In case we are running locally this is the correct location of kuduSync
+      KUDU_SYNC_CMD=kuduSync
+    else
+      # In case we are running on kudu service this is the correct location of kuduSync
+      KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
+    fi
+  fi
+}
+
+# Necessary in the Azure Environment
 selectNodeVersion () {
   if [[ -n "$KUDU_SELECT_NODE_VERSION_CMD" ]]; then
     SELECT_NODE_VERSION="$KUDU_SELECT_NODE_VERSION_CMD \"$DEPLOYMENT_SOURCE\" \"$DEPLOYMENT_TARGET\" \"$DEPLOYMENT_TEMP\""
@@ -83,6 +124,7 @@ sync() {
 
 # MAIN ENTRY POINT
 notifySlack "Stache build started."
+setup
 selectNodeVersion
 install
 build
