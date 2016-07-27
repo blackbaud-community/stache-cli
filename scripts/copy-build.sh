e6361ed31b7ef4e93f4be1ef52f4fd0a17453e61
stevebrush <steve.brush@blackbaud.com>
2016-07-27 11:02:55 -0400

    Added logs to build process.

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -6,24 +6,28 @@ set -e
 echo "TRAVIS_EVENT_TYPE: ${TRAVIS_EVENT_TYPE}";
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
-
-    if [[ $TRAVIS_EVENT_TYPE == 'push' ]]; then
-
-        git config --global user.email "stache-build-user@blackbaud.com"
-        git config --global user.name "Blackbaud Stache Build User"
-
-        git add --all
-        git stash
-        git checkout $STACHE_DEPLOY_BRANCH || git checkout -b $STACHE_DEPLOY_BRANCH
-        rm -rf build
-        git stash pop
-        git add --all
-
-        if ! git diff-index --quiet HEAD --; then
-            git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
-            git push -fq origin $STACHE_DEPLOY_BRANCH
-        fi
-
-    fi
+if [[ $TRAVIS_BRANCH == 'develop' && $TRAVIS_EVENT_TYPE == 'pull_request' ]]; then
+    echo "Commit and push to ${TRAVIS_BRANCH}"
 fi
+
+# if [[ $TRAVIS_BRANCH == 'master' ]]; then
+#
+#     if [[ $TRAVIS_EVENT_TYPE == 'push' ]]; then
+#
+#         git config --global user.email "stache-build-user@blackbaud.com"
+#         git config --global user.name "Blackbaud Stache Build User"
+#
+#         git add --all
+#         git stash
+#         git checkout $STACHE_DEPLOY_BRANCH || git checkout -b $STACHE_DEPLOY_BRANCH
+#         rm -rf build
+#         git stash pop
+#         git add --all
+#
+#         if ! git diff-index --quiet HEAD --; then
+#             git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
+#             git push -fq origin $STACHE_DEPLOY_BRANCH
+#         fi
+#
+#     fi
+# fi
