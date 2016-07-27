63f446d09ae3b5af83b0bf4620693013ee0c03e2
stevebrush <steve.brush@blackbaud.com>
2016-07-27 11:44:08 -0400

    Added logs to build process.

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -7,7 +7,21 @@ echo "TRAVIS_EVENT_TYPE: ${TRAVIS_EVENT_TYPE}";
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 
 if [[ $TRAVIS_BRANCH == 'develop' && $TRAVIS_EVENT_TYPE == 'pull_request' ]]; then
-    echo "Commit and push to ${TRAVIS_BRANCH}"
+    echo "Commit and push to feature-test..."
+
+    git config --global user.email "stache-build-user@blackbaud.com"
+    git config --global user.name "Blackbaud Stache Build User"
+
+    git stash
+    git rm -rf build
+    git stash pop
+    git add --all
+    git status
+
+    if ! git diff-index --quiet HEAD --; then
+        git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
+        git push -fq origin feature-test
+    fi
 fi
 
 # if [[ $TRAVIS_BRANCH == 'master' ]]; then
