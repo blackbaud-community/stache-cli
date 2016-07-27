3ac770b6451666f4360941911674af7517eeb0c1
stevebrush <steve.brush@blackbaud.com>
2016-07-27 12:16:51 -0400

    Updated deployment

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -14,10 +14,15 @@ if [[ $TRAVIS_BRANCH == 'develop' && $TRAVIS_EVENT_TYPE == 'pull_request' ]]; th
     git config --global user.name "Blackbaud Stache Build User"
 
     git add --all
+    git stash
+    git checkout $STACHE_DEPLOY_BRANCH || git checkout -b $STACHE_DEPLOY_BRANCH
+    rm -rf build
+    git stash pop
+    git add --all
 
     if ! git diff-index --quiet HEAD --; then
         git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
-        git push -fq origin feature-test
+        git push -fq origin $STACHE_DEPLOY_BRANCH
     fi
 fi
 
