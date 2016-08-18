0ac342300ca715e1a2b20d38a68b9f660efc9c19
stevebrush <steve.brush@blackbaud.com>
2016-08-18 12:58:24 -0400

    Added commit methods to copy build

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -4,7 +4,6 @@
 set -e
 
 IS_RELEASE=false
-echo "TRAVIS_COMMIT: ${TRAVIS_COMMIT}"
 LAST_COMMIT_MESSAGE=`git log --format=%B -n 1 $TRAVIS_COMMIT`
 
 # Regex matches 'Release vX.X.X' format
@@ -13,10 +12,12 @@ REGEX_RELEASE_COMMENT="^Release v[0-9]+\.[0-9]+\.[0-9]+"
 # Regex matches master,rc-,release
 REGEX_RELEASE_BRANCH="^(master|rc-|release)"
 
-# Is it a git push?
+# Is this commit requesting a release to production?
 if [[ "$TRAVIS_EVENT_TYPE" == "push" ]]; then
+
   # Is the current branch a release-able branch?
   if [[ $TRAVIS_BRANCH =~ $REGEX_RELEASE_BRANCH ]]; then
+
     # Does the commit message match the appropriate pattern?
     if [[ $LAST_COMMIT_MESSAGE =~ $REGEX_RELEASE_COMMENT ]]; then
       IS_RELEASE=true;
@@ -33,59 +34,27 @@ echo "IS_RELEASE: ${IS_RELEASE}"
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   if [[ "$TRAVIS_EVENT_TYPE" == "push" ]]; then
 
+    git config --global user.email "stache-build-user@blackbaud.com"
+    git config --global user.name "Blackbaud Stache Build User"
+
     # push to DEPLOY_TEST_BRANCH
-    echo "push to deploy test branch ${STACHE_DEPLOY_TEST_BRANCH}"
+    echo "Pushing to deployment test branch, ${STACHE_DEPLOY_TEST_BRANCH}..."
+    git add --all
+    git stash
+    git checkout $STACHE_DEPLOY_TEST_BRANCH || git checkout -b $STACHE_DEPLOY_TEST_BRANCH
+    rm -rf build
+    git stash pop
+    git add --all
+    git status
 
-    if [[ "$IS_RELEASE" == "true" ]]; then
+    if ! git diff-index --quiet HEAD --; then
+      git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
+      git push -fq origin $STACHE_DEPLOY_TEST_BRANCH
 
-      # push to DEPLOY_PROD_BRANCH
-      echo "push to deploy prod branch ${STACHE_DEPLOY_PROD_BRANCH}"
+      if [[ "$IS_RELEASE" == "true" ]]; then
+        echo "Pushing to deployment production branch, ${STACHE_DEPLOY_PROD_BRANCH}..."
+        git push -fq origin $STACHE_DEPLOY_PROD_BRANCH
+      fi
     fi
   fi
 fi
-
-# echo "TRAVIS_EVENT_TYPE: ${TRAVIS_EVENT_TYPE}"
-# echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
-#
-#
-# if [[ $TRAVIS_BRANCH == 'develop' && $TRAVIS_EVENT_TYPE == 'pull_request' ]]; then
-#     echo "Commit and push to feature-test..."
-#
-#     git config --global user.email "stache-build-user@blackbaud.com"
-#     git config --global user.name "Blackbaud Stache Build User"
-#
-#     git add --all
-#     git stash
-#     git checkout $STACHE_DEPLOY_BRANCH || git checkout -b $STACHE_DEPLOY_BRANCH
-#     rm -rf build
-#     git stash pop
-#     git add --all
-#     git status
-#
-#     if ! git diff-index --quiet HEAD --; then
-#         git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
-#         git push -fq origin $STACHE_DEPLOY_BRANCH
-#     fi
-# fi
-#
-# # if [[ $TRAVIS_BRANCH == 'master' ]]; then
-# #
-# #     if [[ $TRAVIS_EVENT_TYPE == 'push' ]]; then
-# #
-# #         git config --global user.email "stache-build-user@blackbaud.com"
-# #         git config --global user.name "Blackbaud Stache Build User"
-# #
-# #         git add --all
-# #         git stash
-# #         git checkout $STACHE_DEPLOY_BRANCH || git checkout -b $STACHE_DEPLOY_BRANCH
-# #         rm -rf build
-# #         git stash pop
-# #         git add --all
-# #
-# #         if ! git diff-index --quiet HEAD --; then
-# #             git commit -am "Built via Travis Build #${TRAVIS_BUILD_NUMBER}"
-# #             git push -fq origin $STACHE_DEPLOY_BRANCH
-# #         fi
-# #
-# #     fi
-# # fi
